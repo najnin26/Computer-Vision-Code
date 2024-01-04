a716c915abfc00482ec7cedc6c944f4159572e5d
najnin26 <najninshirin26@gmail.com>
2024-01-04 20:14:24 +0600

    Image read,write and show

--- 1.py
+++ 1.py
@@ -1,6 +1,23 @@
 import cv2
 
 
+img1 = cv2.imread('F:\\CV\\Code\\Data\\avengers.jpg',1)  
+img1 = cv2.resize(img1,(1280,700))#width ,height
+cv2.imshow("Colored Image",img1)  #It accept two parameters 1)- Name of screen ,2) -  Image
+print("Give image with color==\n",img1)
+
+
+img2 =cv2.imread("F:\\CV\\Code\\Data\\avengers.jpg",0)
+img2 =cv2.resize(img2,(1280,700))
+cv2.imshow("Gray scaled image", img2)
+print("Gray image==\n",img2)
+
+
+img3 =cv2.imread("F:\\CV\\Code\\Data\\avengers.jpg",-1)
+img3 =cv2.resize(img3,(1280,700))
+cv2.imshow("Original image", img3)
+print("Original image==\n",img3)
+
 
 
 #Image conversion project colored image into grayscale.
