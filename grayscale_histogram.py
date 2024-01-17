ed4c813dbb5937299c5a88f7799872df4f60cbf4
najnin26 <najninshirin26@gmail.com>
2024-01-17 12:44:08 +0600

    Update grayscale_histogram.py

--- grayscale_histogram.py
+++ grayscale_histogram.py
@@ -5,16 +5,18 @@ import numpy as np
 img = cv.imread("F:\\CV\\Code\\Data\\cats.jpg")
 cv.imshow('Cats', img)
 
-blank=np.zeros(img.shape[:2],dtype='uint8')
-mask=cv.circle(blank, (img.shape[1]//2,img.shape[0]//2), 100,255,-1)
-
-masked=cv.bitwise_and(img, img,mask=mask)
-
-cv.imshow('Mask', masked)
-
 #Gray scale histogram
 gray=cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 cv.imshow('Gray', gray)
+
+blank=np.zeros(img.shape[:2],dtype='uint8')
+mask=cv.circle(blank, (img.shape[1]//2,img.shape[0]//2), 100,255,-1)
+
+masked=cv.bitwise_and(gray, gray,mask=mask)
+
+cv.imshow('Mask', masked)
+
+
 
 gray_hist=cv.calcHist([gray], [0], mask, [256], [0,256])
 
