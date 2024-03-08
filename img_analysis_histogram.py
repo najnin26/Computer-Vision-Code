944b2d98abf2a0bb0f973fe093f560c17dd9d030
najnin26 <najninshirin26@gmail.com>
2024-03-08 20:25:48 +0600

    Update img_analysis_histogram.py

--- img_analysis_histogram.py
+++ img_analysis_histogram.py
@@ -9,11 +9,24 @@ import numpy as np
 import cv2 
 from matplotlib import pyplot as plt
 
-
+"""
 img=np.zeros((200,200),np.uint8)
 
 cv2.rectangle(img, (0,100), (200,200),(255),-1)
 cv2.rectangle(img, (0,50), (50,100),(127),-1)
+
+hist=cv2.calcHist([img], [0], None, [256], [0,256])
+plt.plot(hist)
+plt.show()
+
+cv2.imshow("res", img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+"""
+
+img=cv2.imread("F:\CV\Code\Data\thor.jpg")
+img=cv2.resize(img, (500,500))
+
 
 hist=cv2.calcHist([img], [0], None, [256], [0,256])
 plt.plot(hist)
