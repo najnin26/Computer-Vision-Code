31d552248567312a462047e1abe23eb613ea387b
najnin26 <najninshirin26@gmail.com>
2024-03-25 09:14:23 +0600

    Update Hough_Transformation.py

--- Hough_Transformation.py
+++ Hough_Transformation.py
@@ -1,6 +1,6 @@
 import cv2
 import numpy as np
-
+"""
 img=cv2.imread("F:\CV\Code\Data\chess.jpg")
 img=cv2.resize(img, (400,400))
 gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
@@ -20,6 +20,24 @@ for rho,theta in lines[0]:
     
     cv2.line(img, (x1,y1), (x2,y2), (255,0,255),2)
     
+cv2.imshow('edge', edges)
+cv2.imshow("lines", img)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+"""
+img=cv2.imread("F:\CV\Code\Data\chess.jpg")
+img=cv2.resize(img, (400,400))
+gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+edges=cv2.Canny(gray, 20, 250)
+
+lines=cv2.HoughLinesP(edges, 1, np.pi/180, 100,minLineLength=8,
+                     maxLineGap=100)
+for line in lines:
+    x1,y1,x2,y2 = line[0]
+    cv2.line(img,(x1,y1),(x2,y2),(100,200,125),2)
+
+
 cv2.imshow('edge', edges)
 cv2.imshow("lines", img)
 
