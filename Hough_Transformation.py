c449910b37b66b1241a6ec552fc471eb2de0d3b8
najnin26 <najninshirin26@gmail.com>
2024-03-25 09:20:01 +0600

    Update Hough_Transformation.py

--- Hough_Transformation.py
+++ Hough_Transformation.py
@@ -26,7 +26,7 @@ cv2.imshow("lines", img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 """
-img=cv2.imread("F:\CV\Code\Data\chess.jpg")
+img=cv2.imread("F:\CV\Code\Data\square.png")
 img=cv2.resize(img, (400,400))
 gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 edges=cv2.Canny(gray, 20, 250)
