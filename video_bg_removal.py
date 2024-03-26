45b47f8c148ace948a50384933928006b4525da9
najnin26 <najninshirin26@gmail.com>
2024-03-27 04:24:55 +0600

    Update video_bg_removal.py

--- video_bg_removal.py
+++ video_bg_removal.py
@@ -1,14 +1,23 @@
 import numpy as np
 import cv2 as cv
-cap = cv.VideoCapture("F:\\CV\\Code\\Data\\qqptest2.mp4")
+cap = cv.VideoCapture("F:\\CV\\Code\\Data\\test2.mp4")
 
-alg01=
+algo1=cv.createBackgroundSubtractorMOG2()
+algo2=cv.createBackgroundSubtractorKNN()
+
 while True:
     ret, frame = cap.read()
     frame = cv.resize(frame,(600,400))
     if frame is None:
         break
+    
+    res1=algo1.apply(frame)
+    res2=algo2.apply(frame)
+    
     cv.imshow('original', frame)
+    cv.imshow('res1',res1)
+    cv.imshow('res2',res2)
+    
     keyboard = cv.waitKey(60)
     if keyboard == 'q' or keyboard == 27:
         break
