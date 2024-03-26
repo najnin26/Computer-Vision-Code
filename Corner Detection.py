04709427ef9ed164e233f5af87798e975e381c13
najnin26 <najninshirin26@gmail.com>
2024-03-26 13:16:17 +0600

    Update Corner Detection.py

--- Corner Detection.py
+++ Corner Detection.py
@@ -2,7 +2,10 @@ import numpy as np
 import cv2 as cv
 
 img=cv.imread("F:\CV\Code\Data\shapes.png")
+gray=cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+gray=np.float32(gray)
 
+res=cv.cornerHarris(gray, 2, 3,0.04)
 
 cv.imshow('dst', img)
 
