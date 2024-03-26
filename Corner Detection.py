354e11a59589dabf41e6ab5808b00ca3bcfb50e3
najnin26 <najninshirin26@gmail.com>
2024-03-26 13:20:27 +0600

    Update Corner Detection.py

--- Corner Detection.py
+++ Corner Detection.py
@@ -5,8 +5,10 @@ img=cv.imread("F:\CV\Code\Data\shapes.png")
 gray=cv.cvtColor(img,cv.COLOR_BGR2GRAY)
 gray=np.float32(gray)
 
-res=cv.cornerHarris(gray, 2, 3,0.04)
+res=cv.cornerHarris(gray, 3, 3,0.04)
 
+res=cv.dilate(res, None)
+img[res>0.01*res.max()]=[0,0,255]
 cv.imshow('dst', img)
 
 if cv.waitKey(0) & 0xff == 27:
