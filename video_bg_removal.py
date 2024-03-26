fdf275503502776f642933f3c94a76d54ff7933c
najnin26 <najninshirin26@gmail.com>
2024-03-27 04:26:33 +0600

    Video Background Removal Using Algorithm

--- video_bg_removal.py
+++ video_bg_removal.py
@@ -2,8 +2,8 @@ import numpy as np
 import cv2 as cv
 cap = cv.VideoCapture("F:\\CV\\Code\\Data\\test2.mp4")
 
-algo1=cv.createBackgroundSubtractorMOG2()
-algo2=cv.createBackgroundSubtractorKNN()
+algo1=cv.createBackgroundSubtractorMOG2(detectShadows=True)
+algo2=cv.createBackgroundSubtractorKNN(detectShadows=True)
 
 while True:
     ret, frame = cap.read()
