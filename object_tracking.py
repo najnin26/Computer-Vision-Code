27376184dcfcf9ccfeaecc202a68d8ae41d4a2c7
najnin26 <najninshirin26@gmail.com>
2024-03-28 19:08:53 +0600

    Update object_tracking.py

--- object_tracking.py
+++ object_tracking.py
@@ -14,6 +14,19 @@ cap = cv.VideoCapture("F:\\CV\\Code\\Data\\test2.mp4")
 
 ret, frame = cap.read()
 
+x,y,w,h=580,30,80,150
+track=(x,y,w,h)
+
+roi=frame[y:y+h,x:x+w]
+hsv_roi=cv.cvtColor(roi, cv.COLOR_BGR2HSV)
+
+mask=cv.inRange(hsv_roi,np.array((0.,60.,32.)),np.array((180.,255.,255)))
+roi_hist=cv.calcHist([hsv_roi],[0], mask,[180],[0,180])
+cv.normalize(roi_hist, roi_hist,0,255,cv.NORM_MINMAX)
+
+termntn=(cv.TERM_CRITERIA_EPS|cv.TERM_CRITERIA_COUNT,10,1)
+cv.imshow('roi', roi)
+
 while  True:
     ret, frame = cap.read()
     
