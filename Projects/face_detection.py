8aa372110e932318de55abf5fa4efbb7c8e8a343
najnin26 <najninshirin26@gmail.com>
2024-04-01 20:21:21 +0600

    Update face_detection.py

--- Projects/face_detection.py
+++ Projects/face_detection.py
@@ -11,6 +11,12 @@ faces=face.detectMultiScale(gray,4,4)
 
 for (x,y,w,h) in faces:
     img=cv2.rectangle(img, (x,y),(x+w,y+h), (127,0,205),3)
+    roi_gray=gray[y:y+h,x:x+w]
+    roi_color=img[y:y+h,x:x+w]
+    eyes=eye.detectMultiScale(roi_gray,1.2,1)
+    for(ex,ey,ew,eh) in eyes:
+        cv2.rectangle(roi_color, (ex,ey), (ex+ew,ey+eh),(255,0,0),2)
+    
     
 img=cv2.resize(img, (800,600))
 cv2.imshow('Face Detection', img)
